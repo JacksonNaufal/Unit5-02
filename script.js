758dccb5462fbb3056c6d4f8e497601076362693
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-16 15:35:26 +0000

    Changing quotes to single stinged quotes

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function myhi () {
   document.getElementById('picture').src =
-  "Ocelots.png"
+  'Ocelots.png'
   document.getElementById('background')
-  .style.backgroundColor = "red"
+  .style.backgroundColor = 'red'
 }
 // Picture is the id, which links to Line 16 on the Index.html, which then sees the Ocelots.png, which is the image link, the "background" reads the id for the body on my HTML, which then it tells it for the style, to switch the background color to "red"
 
 function background2 () {
   document.getElementById('background2')
-  .style.backgroundColor= "blue"
+  .style.backgroundColor= 'blue'
 }
 // Background 2 is the id of the button, on the HTML and the document.getElementById, is grabbing the id from the HTML and its telling to to change it to blue.
 
 function para () {
   document.getElementById('para1')
-  .style.color= "blue" 
+  .style.color= 'blue'
 }
 // para is the id of the button, on the HTML and the document.getElementById, is grabbing the id from the HTML and its telling to to change it to blue.
 
