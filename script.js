edabb7a12b884050323d5d96ed3468c99855a94b
Jackson-Naufal <jackson.naufal@ocsbstudent.ca>
2021-03-16 15:30:14 +0000

    spaces

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-function myhi() {
+function myhi () {
   document.getElementById('picture').src =
   "Ocelots.png"
-  document.getElementById ('background')
+  document.getElementById('background')
   .style.backgroundColor = "red"
 }
 // Picture is the id, which links to Line 16 on the Index.html, which then sees the Ocelots.png, which is the image link, the "background" reads the id for the body on my HTML, which then it tells it for the style, to switch the background color to "red"
 
-function background2() {
-  document.getElementById ('background2')
+function background2 () {
+  document.getElementById('background2')
   .style.backgroundColor= "blue"
 }
 // Background 2 is the id of the button, on the HTML and the document.getElementById, is grabbing the id from the HTML and its telling to to change it to blue.
 
-function para() {
-  document.getElementById ('para1')
+function para () {
+  document.getElementById('para1')
   .style.color= "blue" 
 }
 // para is the id of the button, on the HTML and the document.getElementById, is grabbing the id from the HTML and its telling to to change it to blue.
 
-function hi() {
-  document.getElementById ('hello').addEventListener
+function hi () {
+  document.getElementById('hello').addEventListener
   alert("Hello World", hello)
   }
 // the Hello  is the id of =, on the HTML and the document.getElementById, is alerting the addevent listener to find the hello, button on the HTML and its telling to to give an alert everytime it is pressed.
